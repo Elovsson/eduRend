019bb3779ecf1e8e6249cff24da5df1228800134
Elovsson <martin_c_elovsson@hotmail.com>
2024-01-25 17:10:11 +0100

    .

--- src/Cube.cpp
+++ src/Cube.cpp
@@ -1,1 +1,9 @@
 #include "Cube.h"
+
+Cube::Cube(ID3D11Device* dxdevice, ID3D11DeviceContext* dxdevice_context)
+	: Model(dxdevice, dxdevice_context) 
+{
+
+
+
+}
--- src/Cube.h
+++ src/Cube.h
@@ -2,6 +2,14 @@
 #include "model.h"
 class Cube : public Model
 {
+	unsigned m_number_of_indices = 0;
 
+public:
+
+	Cube(ID3D11Device* dxdevice, ID3D11DeviceContext* dxdevice_context);
+
+	~Cube() {}
+
+	virtual void Render() const;
 };
 
