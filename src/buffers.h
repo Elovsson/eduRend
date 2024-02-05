73cb5b73251f1dbe6f9a16a1fb46db8a2391a1bf
Elovsson <martin_c_elovsson@hotmail.com>
2024-02-05 11:48:59 +0100

    buffer

--- src/buffers.h
+++ src/buffers.h
@@ -16,3 +16,11 @@ struct TransformationBuffer
 	linalg::mat4f WorldToViewMatrix; //!< Matrix for converting from world space to view space.
 	linalg::mat4f ProjectionMatrix; //!< Matrix for converting from view space to clip cpace.
 };
+struct LightBuffer 
+{
+	linalg::mat4f 
+};
+struct CameraBuffer 
+{
+
+};
